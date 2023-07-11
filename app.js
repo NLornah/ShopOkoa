9b9a49fbe417c8c7704635eb4ae21b62d38ba4a7
Nderitu Lornah <nderitulornah040@gmail.com>
2023-07-11 12:38:10 +0300

    updates

---
+++ app.js
@@ -0,0 +1,14 @@
+// Select The Elements
+var big_wrapper;
+var hamburger_menu;
+
+function declare() {
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+
